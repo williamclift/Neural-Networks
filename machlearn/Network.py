14f0b3904ee64ba505d4d36da4ece049434ec703
williamclift <cliftjwr@gmail.com>
2020-08-22 19:14:42 -0400

    SGD Function

--- machlearn/Network.py
+++ machlearn/Network.py
@@ -5,6 +5,32 @@
 '''
 import numpy as np
 
+def sigmoid(z):
+	'''Applies Element-wise to numpy array
+	'''
+	return 1.0/(1.0+np.exp(-z))
+
+def feedforward(self, a):
+	'''Return the output of the network if 'a' is input.'''
+	for b, w in zip(self.biases, self.weights):
+		a = sigmoid(np.dot(w, a) + b)
+	return a
+
+def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+	if test_data: n_test = len(test_data)
+	n = len(training_data)
+	for j in range(epochs):
+		random.shuffle(training_data)
+		mini_batches = [
+			training_data[k:k+mini_batch_size]
+			for k in range(0, n, mini_batch_size)]
+		for mini_batch in mini_batches:
+			self.update_mini_batch(mini_batch, eta)
+		if test_data:
+			print "Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test)
+		else:
+			print "Epoch {0} complete".format(j)
+
 class Network(object):
 
 	def __init__(self, sizes):
